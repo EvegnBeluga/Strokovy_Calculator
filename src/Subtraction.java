fc99893362f4a6656eaa31579d100632445caef5
Evgeniy Kozevnikov <104453884+EvegnBeluga@users.noreply.github.com>
2022-07-28 17:30:49 +0300

    Update Subtraction.java

--- src/Subtraction.java
+++ src/Subtraction.java
@@ -4,12 +4,13 @@ public class Subtraction {
             System.out.println("Ошибка:  колличество символов операнда не должно привышать 10ти!!!");
             System.exit(22);
         }
-        String sub;
+        String[] arr = new String[second.length()];
+        String[] ass = new String[first.length()];
+        String sub = "";
         if (first.contains(second)) {
-            int difference = first.length() - second.length();
-            sub = first.substring(0, difference);
-        } else {
-            sub = first;
+            int fir = first.indexOf(second);
+            int k = fir + arr.length;
+            sub = first.substring(0, fir) + first.substring(k, ass.length);
         }
         return sub;
     }
